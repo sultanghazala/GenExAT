615546c3dededb470a0fa6c7f63102d8958664f9
Ghazala Sultan <89488854+sultanghazala@users.noreply.github.com>
2024-07-03 18:28:04 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -467,10 +467,10 @@ fluidPage(theme = shinytheme("cerulean"), # style='border-right: 1px solid black
                                  ## =========================================================================== ##
                                  ## Visualization TABS
                                  ## =========================================================================== ##
-                                 source("ui-tab-dotplot.R",local=TRUE)$value,
+                                 source("ui-tab-samplegroupplots.R",local=TRUE)$value,
                                  source("ui-tab-analysisres.R",local=TRUE)$value,
                                  source("ui-tab-heatmap.R",local=TRUE)$value,
-                                 source("ui-tab-samplegroupplots.R",local=TRUE)$value,
+                                 source("ui-tab-dotplot.R",local=TRUE)$value,
                                )  # nav
                       ), #tabPanel main Data Analysis
                          

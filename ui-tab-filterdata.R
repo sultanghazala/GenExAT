a89078ba37b0390a0fa39176443e880d1cf3f709
Ghazala Sultan <89488854+sultanghazala@users.noreply.github.com>
2024-07-03 17:33:33 +0530

    Update ui-tab-filterdata.R

--- ui-tab-filterdata.R
+++ ui-tab-filterdata.R
@@ -1,12 +1,12 @@
 ## ==================================================================================== ##
-# App Name: GeExAT - Gene Expression Analysis Tool
+# App Name: GenExAT - Gene Expression Analysis Tool
 # Author: Ghazala Sultan & Swaleha Zubair from Department of Computer Science, AMU, Aligarh, India.
 #
 # This is a Shiny web application with All Rights Reserved to aforementioned Author.
 # You may contact the author of this code, Ghazala Sultan at <gsultan@myamu.ac.in>
 ## ==================================================================================== ##
 
-tabPanel("Filter Data", 
+tabPanel("Data Filter", 
          ## ==================================================================================== ##
          ## Left hand column has the filtera settings and options
          ## ==================================================================================== ##

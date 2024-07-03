48db50058a9c681534ce82bba5aa09e2c3ba1f17
Ghazala Sultan <89488854+sultanghazala@users.noreply.github.com>
2024-07-03 17:37:30 +0530

    Update ui-tab-dotplot.R

--- ui-tab-dotplot.R
+++ ui-tab-dotplot.R
@@ -1,12 +1,12 @@
 ## ==================================================================================== ##
-# App Name: GeExAT - Gene Expression Analysis Tool
+# App Name: GenExAT - Gene Expression Analysis Tool
 # Author: Ghazala Sultan & Swaleha Zubair from Department of Computer Science, AMU, Aligarh, India.
 #
 # This is a Shiny web application with All Rights Reserved to aforementioned Author.
 # You may contact the author of this code, Ghazala Sultan at <gsultan@myamu.ac.in>
 ## ==================================================================================== ##
 
-tabPanel("Gene Expression Boxplots",
+tabPanel("PCA (Principal Component Analysis)",
          fluidRow(column(4,wellPanel(
            tags$head( tags$style("body {background-color: white; }")),
            selectizeInput("sel_gene",
